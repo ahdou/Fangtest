f898b0d8a5ccee9e8e3e4c0813d45996f040e4d5
ahdou <mail@ahdou.info>
2015-10-10 17:13:04 +0800

    10-10修改版

---
+++ js/orbit.js
@@ -0,0 +1,15 @@
+$('#featured').orbit({
+frontendWidth: 740,		//frontend Img Width
+frontendHeight: 500,
+frontendLeft: 238,  //front left
+backendWidth: 400, //backend Img Width 1
+backendTop: 20,	//backend Img  1 Position
+backendLeft: 800, //backend Img  1 left
+backendLeft3: 20, //backend Img  1 rgiht
+backendWidth2: 400, //backend Img Width 2
+backendTop2: 20,	// backend Img Position 2      
+backendLeft4: 780,// backend Img Position right 
+backendLeft2:600, //backend Img Position left
+sceneWidth: 1200,
+sceneHeight: 500,
+});	
